a9572c6497d6deb61ea97c3628e39ed7cc28a509
admdrew <admdrew@gmail.com>
2013-10-03 16:19:07 -0500

    initial commit
    parses xml and builds treeview

---
+++ MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace IUSDatasources {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialogXml = new System.Windows.Forms.OpenFileDialog();
+            this.btnParseFile = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.tvXML = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(87, 12);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(343, 20);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(69, 20);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Select File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // openFileDialogXml
+            // 
+            this.openFileDialogXml.FileName = "openFileDialogXml";
+            // 
+            // btnParseFile
+            // 
+            this.btnParseFile.Location = new System.Drawing.Point(436, 11);
+            this.btnParseFile.Name = "btnParseFile";
+            this.btnParseFile.Size = new System.Drawing.Size(68, 21);
+            this.btnParseFile.TabIndex = 2;
+            this.btnParseFile.Text = "Parse XML";
+            this.btnParseFile.UseVisualStyleBackColor = true;
+            this.btnParseFile.Click += new System.EventHandler(this.btnParseFile_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(87, 39);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "label1";
+            // 
+            // tvXML
+            // 
+            this.tvXML.Location = new System.Drawing.Point(12, 66);
+            this.tvXML.Name = "tvXML";
+            this.tvXML.Size = new System.Drawing.Size(492, 386);
+            this.tvXML.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 464);
+            this.Controls.Add(this.tvXML);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnParseFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialogXml;
+        private System.Windows.Forms.Button btnParseFile;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TreeView tvXML;
+
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace IUSDatasources {
+    public partial class MainForm : Form {
+        public static string STR_FILENAME_XML;
+        
+
+        public MainForm() {
+            InitializeComponent();
+            txtFileName.Text = "C:\\_QA\\IUS\\win2012-worksite-web_datasources.xml.txt";
+            lblOutput.Text = "(output)";
+            //tvXML.Dock = DockStyle.Fill;
+            tvXML.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top;
+        }
+
+        // Select file
+        private void btnOpenFile_Click(object sender, EventArgs e) {
+            OpenFileDialog openFileDialog_XML = new OpenFileDialog();
+            //string STR_FILENAME_XML;
+
+            openFileDialog_XML.InitialDirectory = "c:\\";
+            openFileDialog_XML.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+            //openFileDialog_XML.FilterIndex = 2;
+            openFileDialog_XML.RestoreDirectory = true;
+            openFileDialog_XML.Multiselect = false;
+
+            if (openFileDialog_XML.ShowDialog() == DialogResult.OK) {
+                //STR_FILENAME_XML = openFileDialog_XML.FileName;
+                txtFileName.Text = openFileDialog_XML.FileName;
+
+                //txtFileName.Text = STR_FILENAME_XML;
+            }
+        }
+
+        // Read in file
+        private void btnParseFile_Click(object sender, EventArgs e) {
+            STR_FILENAME_XML = txtFileName.Text;
+
+            if (!string.IsNullOrWhiteSpace(STR_FILENAME_XML)) {
+                //lblOutput.Text = "good to GO";
+                parseXML(STR_FILENAME_XML);
+            }
+            else {
+                lblOutput.Text = "empty";
+            }
+        }
+
+        // Parse xml
+        private void parseXML(string strFileName) {
+            // load xml into doc
+            //XmlReader reader = XmlReader.Create(strFileName);
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(strFileName);
+
+            // init treeview
+            tvXML.Nodes.Clear();
+            tvXML.Nodes.Add(new TreeNode(xmldoc.DocumentElement.Name));
+            TreeNode tNode = new TreeNode();
+            tNode = tvXML.Nodes[0];
+
+            // populate treeview
+            AddNodes(xmldoc.DocumentElement, tNode);
+            tvXML.ExpandAll();
+        }
+
+        // taken from http://support.microsoft.com/kb/317597
+        // recurse the SHIT out of the XML tree
+        private void AddNodes(XmlNode inXmlNode, TreeNode inTreeNode) {
+            XmlNode xNode;
+            TreeNode tNode;
+            XmlNodeList xNodeList;
+
+            // loop down through XML to find leaf (adding to inTreeNode as we traverse)
+            if (inXmlNode.HasChildNodes) {
+                xNodeList = inXmlNode.ChildNodes;
+                for (int i = 0; i <= xNodeList.Count - 1; i++) {
+                    xNode = inXmlNode.ChildNodes[i];
+                    inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+                    tNode = inTreeNode.Nodes[i];
+                    AddNodes(xNode, tNode);
+                }
+            }
+            // "Here you need to pull the data from the XmlNode based on the type of node, whether attribute values are required, and so forth."
+            // meh
+            else {
+                inTreeNode.Text = (inXmlNode.OuterXml).Trim();
+            }
+        }
+    }
+}
